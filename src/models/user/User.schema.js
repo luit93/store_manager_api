dd44f5a11dc3ffcec6dcac13d23c4eb6aeac7420
luit saikia <luit93@gmail.com>
2022-07-08 05:41:01 +1000

    added user schema

---
+++ src/models/user/User.schema.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// defining user schema as a function
+const UserSchema = new Schema({
+  name: { type: String, maxLength: 50, required: true },
+  email: { type: String, maxLength: 50, required: true },
+  phone: { type: Number, maxLength: 11, required: false },
+  password: { type: String, maxLength: 100, minLength: 8, required: true },
+});
+
+module.exports = {
+    UserSchema: mongoose.model("User",UserSchema)
+}
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,14 +1,24 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const { insertUser } = require("../models/user/User.model");
+const { hashPassword } = require("../helpers/bcrypt.helper");
+router.all("/", (req, res, next) => {
+  // res.json({message:"returned from user router"})
+  next();
+});
+router.post("/", async (req, res) => {
+  // console.log(req.body)
+  const { name, email, phone, password } = req.body;
 
-router.all('/',(req,res,next)=>{
-    
-    // res.json({message:"returned from user router"})
-    next()
-})
-router.post("/",(req,res)=>{
-    console.log(req.body)
-    res.json(req.body)
-})
+  try {
+    const hashedPassword = await hashPassword(password);
+    const newUser = { name, email, phone, password: hashedPassword };
+    const result = await insertUser(newUser);
+    console.log(result);
+    res.json({ message: "New user has been created", result });
+  } catch (error) {
+    res.json({ message: error.message, status: "error" });
+  }
+});
 
-module.exports = router
+module.exports = router;
